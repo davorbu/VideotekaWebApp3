e4cf9ad058b27a4524c097432da7ba834d735769
Davor Burmudžija <davorburmudzija@gmail.com>
2021-02-14 14:56:00 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideotekaWebApp3.Models;
+
+namespace VideotekaWebApp3.Controllers
+{
+    public class HomeController : Controller
+    {   
+
+        private List<Film> DohvatiFilmoveIzSessiona()
+        {
+            List<Film>filmovi = new List<Film>();
+            filmovi = Session["filmovi"] as List<Film>;
+            if (filmovi == null)
+            {
+                filmovi = new Film().DohvatiFilmove(50);
+                Session["filmovi"] = filmovi;
+            }
+
+            return filmovi;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("dohvatilistu/{count}")]
+        public ActionResult ListaFilmova(int count)
+        {
+            //Film film = new Film();
+
+            return PartialView(DohvatiFilmoveIzSessiona());
+        }
+
+
+
+        [HttpGet]
+        [Route("dohvatilistupaginacija/{page}")]
+        public ActionResult ListaFilmovaPaginacija(int page)
+        {        
+           var filmovi = DohvatiFilmoveIzSessiona().Skip(page).Take(10).ToList();
+          return PartialView("ListaFilmova",filmovi);
+        }
+
+
+        [HttpGet]
+        [Route("filmjson/{id}")]
+        public ActionResult FilmJson(int id)
+        {        
+           var film = DohvatiFilmoveIzSessiona().FirstOrDefault(x=>x.Id == id);
+          return Json(film, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Models/Film.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideotekaWebApp3.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+
+        public string Naslov { get; set; }
+
+        public string Zanr { get; set; }
+
+        public DateTime Godina { get; set; }
+
+        
+        public List<Film>DohvatiFilmove(int count)
+        {
+            List<Film>filmovi = new List<Film>();
+            Random random = new Random();
+
+            for(int i = 0; i < count; i++)
+            {
+                filmovi.Add(
+                    new Film
+                    {
+                        Godina = DateTime.Now.AddYears((random.Next(1,5)) * -1),
+                        Id = i,
+                        Naslov = "Naslov" + random.Next(i, 50),
+                        Zanr = "Zanr" + random.Next(i, 50)
+
+
+                    });
+
+            }
+            return filmovi;
+        }
+
+
+    }
+
+
+
+    
+
+
+}
